560f3b7b57b276fc93b6db08fbc4e8fe21c4f09f
Michael Spitznagel <exultavit@gmail.com>
2015-07-31 01:28:28 -0400

    Create types for library interface
    The basic interface types are VarReader and VarWriter.
    
    I have no plans for a mixed-mode ReadWriter.

--- matfile.go
+++ matfile.go
@@ -5,3 +5,28 @@
 // Package matfile implements the encoding and decoding of v5 MAT-File data.
 package matfile
 
+// VarReader represents a single header and a sequence of decodable variables
+type VarReader interface {
+	GetHeader() (Header, error)
+	GetVarInfo() (VarInfo, error)
+	GetVar() (Var, error)
+	Next() error
+}
+
+// VarWriter encodes variables sequentially
+type VarWriter interface {
+	PutVar(Var)
+}
+
+// Header contains descriptive text, a version, and a byte-order indicator
+type Header struct {
+}
+
+// VarInfo describes the name and type of a Var
+type VarInfo struct {
+}
+
+// Var is the basic unit of data decoded from a File
+type Var struct {
+	VarInfo
+}
