42298df6087d201182175abf0308c69152ea15f6
Michael Spitznagel <exultavit@gmail.com>
2015-07-31 01:27:31 -0400

    Add copyright notice

--- matfile.go
+++ matfile.go
@@ -1,3 +1,7 @@
+// Copyright 2015 Michael Spitznagel.
+// This is program is free software.  You may distribute it under the
+// terms of the GNU General Public License.
+
 // Package matfile implements the encoding and decoding of v5 MAT-File data.
 package matfile
 
