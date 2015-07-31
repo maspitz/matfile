4bbafe9505cb09f47af327e6c09d1f21c2d81930
Michael Spitznagel <exultavit@gmail.com>
2015-07-31 02:12:16 -0400

    Create placeholder tests
    I intend the tests typically to interact with the package via its
    exported interface.  So I have made the initial placeholders correspond
    to specific interface methods.
    
    However, the tests will eventually be organized more by data type being
    read.  So these tests will all change once their content is actually written.

---
+++ matfile_test.go
@@ -0,0 +1,38 @@
+// Copyright 2015 Michael Spitznagel.
+// This is program is free software.  You may distribute it under the
+// terms of the GNU General Public License.
+
+package matfile
+
+import "testing"
+
+// To test *decoding*, we read data from a file of known content
+// created by GNU Octave.
+
+// To test *encoding*, we encode data, then decode it and check consistency.
+
+// TestHeader is a placeholder for tests of more specific functionality
+func TestHeader(t *testing.T) {
+	t.Errorf("Header not implemented")
+}
+
+// TestGetVarInfo is a placeholder for tests of more specific functionality
+func TestGetVarInfo(t *testing.T) {
+	t.Errorf("GetVarInfo not implemented")
+}
+
+// TestGetVar is a placeholder for tests of more specific functionality
+func TestGetVar(t *testing.T) {
+	t.Errorf("GetVar not implemented")
+}
+
+// TestNext is a placeholder for tests of more specific functionality
+func TestNext(t *testing.T) {
+	t.Errorf("Next not implemented")
+}
+
+// TestPutVar is a placeholder for tests of more specific functionality
+func TestPutVar(t *testing.T) {
+	t.Errorf("PutVar not implemented")
+}
+
