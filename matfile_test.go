703d08ca3b63c76ab3cd89285ef4416e4d79c920
Michael Spitznagel <exultavit@gmail.com>
2015-07-31 16:45:53 -0400

    Remove placeholder tests
    I decided that making non-specific placeholder tests that
    always fail was actually worse than useless.
    
    My new policy is to make a specific test at the time when I
    intend to add specific functionality.

--- matfile_test.go
+++ matfile_test.go
@@ -13,26 +13,6 @@ import "testing"
 
 // TestHeader is a placeholder for tests of more specific functionality
 func TestHeader(t *testing.T) {
-	t.Errorf("Header not implemented")
-}
-
-// TestGetVarInfo is a placeholder for tests of more specific functionality
-func TestGetVarInfo(t *testing.T) {
-	t.Errorf("GetVarInfo not implemented")
-}
-
-// TestGetVar is a placeholder for tests of more specific functionality
-func TestGetVar(t *testing.T) {
-	t.Errorf("GetVar not implemented")
-}
-
-// TestNext is a placeholder for tests of more specific functionality
-func TestNext(t *testing.T) {
-	t.Errorf("Next not implemented")
-}
-
-// TestPutVar is a placeholder for tests of more specific functionality
-func TestPutVar(t *testing.T) {
-	t.Errorf("PutVar not implemented")
+//	t.Errorf("Header not implemented")
 }
 
